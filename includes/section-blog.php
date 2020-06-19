29a1871c32a4ed2801e923a5b74c7eea297bba04
Sean <seanreilly123@hotmail.com>
2020-06-19 16:59:58 +1200

    Padding added to category and region on single page

--- includes/section-blog.php
+++ includes/section-blog.php
@@ -6,21 +6,24 @@ if (have_posts()) :
         $terms = get_the_terms($post->ID, 'regions');
         if ($terms) :
             foreach ($terms as $term) { ?>
-
-                <a class="text-dark" href="<?php echo get_term_link($term->term_id, 'regions'); ?>">
-                    <?php echo "<p>Region: " . $term->name . "</p>"; //go and add single_cat_title() in archive 
-                    ?>
-                </a>
+                <p class="mt-3">
+                    <a class="text-dark" href="<?php echo get_term_link($term->term_id, 'regions'); ?>">
+                        <?php echo "Region: " . $term->name; //go and add single_cat_title() in archive 
+                        ?>
+                    </a>
+                </p>
             <?php
             }
         endif;
         $categories = get_the_category();
         if ($categories) :
             foreach ($categories as $cat) : ?>
-                <a class="text-dark" href="<?php echo get_category_link($cat->term_id); ?>">
-                    <?php echo "<p>Categories: " . $cat->name . "</p>"; //go and add single_cat_title() in archive 
-                    ?>
-                </a>
+                <p class="mt-3">
+                    <a class="text-dark" href="<?php echo get_category_link($cat->term_id); ?>">
+                        <?php echo "Categories: " . $cat->name; //go and add single_cat_title() in archive 
+                        ?>
+                    </a>
+                </p>
 <?php endforeach;
         endif;
     endwhile;
